a9b77baa5cba252c0f5000214b51dd563d8797fb
Vinicius Tirabassi <vinicius@Viniciuss-MacBook-Air.local>
2022-04-04 13:48:41 -0300

    first commit

---
+++ grpc-unary/client/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Dummy;
+using System.Threading.Tasks;
+using System;
+using Greet;
+
+const string _target = "127.0.0.1:50051";
+
+var channel = new Channel(_target, ChannelCredentials.Insecure);
+
+channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+        Console.WriteLine("Client connected with success");
+});
+
+//var client = new DummyService.DummyServiceClient(channel);
+
+var client = new GreetingSerivce.GreetingSerivceClient(channel);
+
+var greeting = new Greeting()
+{
+    FirstName = "Vinicius",
+    LastName = "Oliveira"
+};
+
+var request = new GreetingRequest()
+{
+    Greeting = greeting
+};
+
+var response = client.Greet(request);
+
+Console.WriteLine(response.Result);
+
+channel.ShutdownAsync().Wait();
+
+Console.ReadKey();
---
+++ grpc-unary/server/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Greet;
+using Grpc.Core;
+using server;
+using System;
+using System.IO;
+
+const int _port = 50051;
+
+Server server = null;
+
+try
+{
+    server = new Server()
+    {
+        Services = { GreetingSerivce.BindService(new GreetingServiceImpl())},
+        Ports = { new ServerPort("localhost", _port, ServerCredentials.Insecure) }
+    };
+
+    server.Start();
+    Console.WriteLine($"Server listening on port {_port}");
+    Console.ReadKey();
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"Server failed to start, message: {ex.Message}");
+    throw;
+}
+finally
+{
+    if (server is not null)
+        server.ShutdownAsync().Wait();
+
+    Console.WriteLine($"Finish Server");
+}
+
